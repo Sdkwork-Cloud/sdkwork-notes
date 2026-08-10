b1052568fb948d67e202387d7027ae794e62ee85
zowork <sdkwork@zowalk.com>
2026-08-10 19:06:52 +0800

    chore: sync local changes

--- database/migrations/postgres/0001_organization_id_not_null.up.sql
+++ database/migrations/postgres/0001_organization_id_not_null.up.sql
@@ -18,30 +18,37 @@
 
 BEGIN;
 
+ALTER TABLE notes_workspace ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_workspace SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_workspace ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_workspace ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_page ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_page SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_page ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_page ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_page_search_projection ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_page_search_projection SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_page_search_projection ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_page_search_projection ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_ai_job ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_ai_job SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_ai_job ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_ai_job ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_ai_job_source ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_ai_job_source SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_ai_job_source ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_ai_job_source ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_ai_suggestion ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_ai_suggestion SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_ai_suggestion ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_ai_suggestion ALTER COLUMN organization_id SET NOT NULL;
 
+ALTER TABLE notes_ai_feedback ADD COLUMN IF NOT EXISTS organization_id TEXT NOT NULL DEFAULT '0';
 UPDATE notes_ai_feedback SET organization_id = '0' WHERE organization_id IS NULL;
 ALTER TABLE notes_ai_feedback ALTER COLUMN organization_id SET DEFAULT '0';
 ALTER TABLE notes_ai_feedback ALTER COLUMN organization_id SET NOT NULL;
